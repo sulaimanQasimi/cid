44898c2a3174d590d0a85e1a5999990012472c63
Mohammad  Rafi10 <87257987+sulaimanQasimi@users.noreply.github.com>
2025-08-06 11:22:44 +0430

    aa

---
+++ public/build1/assets/Animated-C36gg0dH.js
@@ -0,0 +1,1 @@
+import{A as m}from"./AnimatedTheme-j3JkNzt2.js";import"./Theme-CRx2HxaY.js";export{m as default};
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
 import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
+import Checker from 'vite-plugin-checker';
 
 export default defineConfig({
     plugins: [
@@ -13,6 +14,8 @@ export default defineConfig({
         }),
         react(),
         tailwindcss(),
+
+        Checker({ typescript: true }),
     ],
     esbuild: {
         jsx: 'automatic',
