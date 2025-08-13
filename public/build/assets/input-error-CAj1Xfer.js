8d0183dd85a1a43bceaa4e1f91cd9e0ec162830e
Mohammad  Rafi10 <87257987+sulaimanQasimi@users.noreply.github.com>
2025-08-13 10:59:06 +0430

    106 Publish

---
+++ public/build/assets/input-error-CAj1Xfer.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-Cnwq3aGu.js";import{c as o}from"./utils-nIk1e_UI.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
