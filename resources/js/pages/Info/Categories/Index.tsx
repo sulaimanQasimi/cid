8cac84410daf7b1f72261ff1e748bde37ec32e25
Mohammad  Rafi10 <152247100+Mohammadrafi10@users.noreply.github.com>
2025-09-08 10:00:01 +0430

    Remove unused Badge import from Info Categories Index component for code cleanliness

--- resources/js/pages/Info/Categories/Index.tsx
+++ resources/js/pages/Info/Categories/Index.tsx
@@ -12,7 +12,6 @@ import {
   TableHeader,
   TableRow
 } from '@/components/ui/table';
-import { Badge } from '@/components/ui/badge';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import {
   AlertDialog,
