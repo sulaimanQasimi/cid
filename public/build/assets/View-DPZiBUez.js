2bacacd596cd36fe71980341166348ba47023fb9
Mohammad  Rafi10 <farzadqasimy@gmail.com>
2025-09-09 11:24:06 +0430

    Remove deprecated 'hot' file from public directory

---
+++ public/build/assets/View-DPZiBUez.js
@@ -0,0 +1,1 @@
+import{j as t,L as r}from"./app-D74mREVM.js";import{u as s}from"./translate-r61kUS1h.js";function a(){const{t:e}=s();return t.jsx(t.Fragment,{children:t.jsx(r,{title:e("reports.view.page_title",{code:""})})})}export{a as default};
