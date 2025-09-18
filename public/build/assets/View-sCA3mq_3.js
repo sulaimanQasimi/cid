14e8a7a1c54d8799089d40d5dc34178c96e5407d
Mohammad  Rafi10 <farzadqasimy@gmail.com>
2025-09-18 11:39:05 +0430

    Remove deprecated development server URL from public/hot file

---
+++ public/build/assets/View-sCA3mq_3.js
@@ -0,0 +1,1 @@
+import{j as t,L as r}from"./app-BoRYl7ul.js";import{u as s}from"./translate-Ci5XD_xD.js";function a(){const{t:e}=s();return t.jsx(t.Fragment,{children:t.jsx(r,{title:e("reports.view.page_title",{code:""})})})}export{a as default};
