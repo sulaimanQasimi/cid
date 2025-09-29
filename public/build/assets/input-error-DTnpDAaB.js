a42a702c8cc6d1b66a4ac5a85e5e5b1e71baa557
Mohammad  Rafi10 <farzadqasimy@gmail.com>
2025-09-29 09:55:06 +0430

    publish

--- public/build/assets/input-error-DTnpDAaB.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-BoRYl7ul.js";import{c as o}from"./utils-CAQgsxix.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
