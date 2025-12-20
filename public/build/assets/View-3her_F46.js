84cabab5ae7e4bdd865178080ffce146e88badd7
SulaimanQasimi <farzadqasimy@gmail.com>
2025-12-20 11:38:47 +0430

    build completed

---
+++ public/build/assets/View-3her_F46.js
@@ -0,0 +1,1 @@
+import{j as t,L as r}from"./app-CYvH7qVq.js";import{u as s}from"./translate-DGEDEJ-H.js";function a(){const{t:e}=s();return t.jsx(t.Fragment,{children:t.jsx(r,{title:e("reports.view.page_title",{code:""})})})}export{a as default};
--- public/build/assets/input-error-BWNV4Mo8.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-3tDC9xQW.js";import{c as o}from"./utils-D9L8Nuxo.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
